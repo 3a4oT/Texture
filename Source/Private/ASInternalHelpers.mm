0aca104a0aa295df9004d2c3d80d76f92a72839d
Oleksandr Svatiuk <alsv82@gmail.com>
2025-04-01 20:16:10 +0300

    Fixed deadlock according with the described approach in https://github.com/TextureGroup/Texture/issues/2029

--- Source/Private/ASInternalHelpers.mm
+++ Source/Private/ASInternalHelpers.mm
@@ -70,9 +70,9 @@ void ASInitializeFrameworkMainThread(void)
     if (ASActivateExperimentalFeature(ASExperimentalLayerDefaults)) {
       // Nop. We will gather default values on-demand in ASDefaultAllowsGroupOpacity and ASDefaultAllowsEdgeAntialiasing
     } else {
-      CALayer *layer = [[[UIView alloc] init] layer];
-      allowsGroupOpacityFromUIKitOrNil = @(layer.allowsGroupOpacity);
-      allowsEdgeAntialiasingFromUIKitOrNil = @(layer.allowsEdgeAntialiasing);
+      // Use default values here instead of loading the layer
+      allowsGroupOpacityFromUIKitOrNil = @(YES);
+      allowsEdgeAntialiasingFromUIKitOrNil = @(NO);
     }
     ASNotifyInitialized();
 #if AS_SIGNPOST_ENABLE
