eec1b0b3e7f738ada534b87ca0955ec9fa3131e7
Petro Rovenskyi <petro.rovenskyy@gmail.com>
2026-04-01 16:10:02 +0300

    Fix CI: use iOS 26.2 simulator and Xcode 26.3 (26.4 runtime not installed on runner)

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@
 # echo ************* diagnostics end
 
 # run this on a 2x device until we've updated snapshot images to 3x
-PLATFORM="${TEXTURE_BUILD_PLATFORM:-platform=iOS Simulator,OS=26.4,name=iPhone 17}"
-SDK="${TEXTURE_BUILD_SDK:-iphonesimulator26.4}"
+PLATFORM="${TEXTURE_BUILD_PLATFORM:-platform=iOS Simulator,OS=26.2,name=iPhone 16}"
+SDK="${TEXTURE_BUILD_SDK:-iphonesimulator26.2}"
 DERIVED_DATA_PATH="~/ASDKDerivedData"
 
 # It is pitch black.
